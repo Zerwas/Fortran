d12f71d2bf53804509605c63e3716a3183405725
Zerwas <f_starke@gmx.de>
2015-07-23 17:36:28 +0200

    klausurvorbeireitung

--- 07_Klausur_Derivation.f95
+++ 07_Klausur_Derivation.f95
@@ -6,42 +6,46 @@ module derivation
 
 	interface operator (+)
 		module procedure add
-	end interface (+)
+	end interface operator (+)
 
 	interface operator (-)
 		module procedure sub
-	end interface (-)
+	end interface operator (-)
 
 	interface operator (*)
 		module procedure mul
-	end interface (*)
+	end interface operator (*)
 
 	interface operator (/)
 		module procedure div
-	end interface (/)
+	end interface operator (/)
 
 	contains
 
 	function add(diff1,diff2)
-		type(diff)::diff1,diff2,add
+		type(diff),intent(in)::diff1,diff2
+		type(diff)::add
 		add%value=diff1%value+diff2%value
 		add%deriv=diff1%deriv+diff2%deriv
 	end function add
 
 	function sub(diff1,diff2)
-		type(diff)::diff1,diff2,add
+		type(diff),intent(in)::diff1,diff2
+		type(diff)::sub
 		sub%value=diff1%value-diff2%value
 		sub%deriv=diff1%deriv-diff2%deriv
 	end function sub
 
 	function mul(diff1,diff2)
-		type(diff)::diff1,diff2,add
+		type(diff),intent(in)::diff1,diff2
+		type(diff)::mul
 		mul%value=diff1%value*diff2%value
 		mul%deriv=diff1%deriv*diff2%value+diff1%value*diff2%deriv
 	end function mul
 
 	function div(diff1,diff2)
-		type(diff)::diff1,diff2,add
+		type(diff),intent(in)::diff1,diff2
+		type(diff)::div
 		div%value=diff1%value/diff2%value
 		div%deriv=(diff1%deriv-(diff1%value/diff2%value)*diff2%deriv)/diff2%value
 	end function div
@@ -61,15 +65,16 @@ module derivation
 	end function diffvar
 
 	subroutine put(diff1)
-		write(*,*) 'f(x)=',diff%value
-		write(*,*) 'f\'(x)=',diff%deriv
+		type(diff)::diff1
+		write(*,*) 'f(x)=',diff1%value
+		write(*,*) 'fp(x)=',diff1%deriv
 		write(*,*) ''
 	end subroutine
 
 	function f(x)
 		real::x
 		type(diff)::f
-		f=(diffvar(x)+diffcon(4))/(diffvar(x)*diffvar(x)+diffcon(5))
+		f=(diffvar(x)+diffcon(4.0))/(diffvar(x)*diffvar(x)+diffcon(5.0))
 	end function f
 
 end module derivation
